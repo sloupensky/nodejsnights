c4fb1112ec0a080b99d4d6a5d932c17c58fd0621
Michal Sloupenský <michal.sloupensky@previo.cz>
2018-10-10 07:03:05 +0200

    Homework 01 refactoring

--- homework-01.js
+++ homework-01.js
@@ -7,11 +7,18 @@ const BASE_URL = 'http://swapi.co/api'
 async function run() {
     try {
         const result = await requestPromise(`${BASE_URL}/people/1`)
-        console.log('ASYNC function')
-        console.log(JSON.parse(result))
-        console.log('----')
+        const resultObj = JSON.parse(result)
+
+        for(vehicleUrl of resultObj.vehicles) {
+            const vehicle = await requestPromise(vehicleUrl)
+            
+            console.log('ASYNC function')
+            console.log(JSON.parse(vehicle)) 
+            console.log('----')
+        }
     } catch(err) {
         console.log('Error async')
+        console.log(err)
         console.log('----')
     } 
 }
@@ -20,23 +27,41 @@ run()
 
 // Promises
 requestPromise(`${BASE_URL}/people/1`).then((data) => {
-    console.log('Promises')
-    console.log(JSON.parse(data))
-    console.log('----')
-}).catch(() => {
+    const resultObj = JSON.parse(data)
+
+    Promise.all(resultObj.vehicles.map(url => requestPromise(url))).then(data => {
+        console.log('Promises')
+        for(vehicle of data) {
+            console.log(JSON.parse(vehicle))
+        }
+        console.log('----')
+    })
+}).catch((err) => {
     console.log('Error promise')
+    console.log(err)
     console.log('----')
 })
 
 // Callback
-request(`${BASE_URL}/people/1`,{json : true}, (err, response, data) => {
+request(`${BASE_URL}/people/1`,{json : true}, (err, response, result) => {
     if(response.statusCode !== 200 || err) {
         console.log(`Error callback ${response.statusCode}`)
         console.log('----')
 
         return
     }
-    console.log('Callback')
-    console.log(data);
-    console.log('----')
+
+    for (vehicleUrl of result.vehicles) {
+        request(vehicleUrl,{json : true}, (err, response, result) => {
+            if(response.statusCode !== 200 || err) {
+                console.log(`Error callback ${response.statusCode}`)
+
+                return
+            }
+
+            console.log('Callback')
+            console.log(result)
+            console.log('----')
+        })
+    }
 })
